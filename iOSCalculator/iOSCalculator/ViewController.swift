7f40868f2585d7275dd0cb218d6d3cde86ca87ee
Seo <kirin2211@naver.com>
2023-07-05 18:46:00 +0900

    Optional, Conditional

--- iOSCalculator/iOSCalculator/ViewController.swift
+++ iOSCalculator/iOSCalculator/ViewController.swift
@@ -9,15 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func touchDigit(_ sender: UIButton) {
-        print("touchDigit!")
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+            
+        }
+        else{
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
     
-
+    @IBAction func performOperation(_ sender: UIButton) {
+        if let mathmaticalSymbol = sender.currentTitle {
+            if mathmaticalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+        
+    }
+    
 }
 

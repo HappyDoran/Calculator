1bf40304f7f37bae735ffc5ea6b13bbd2a9a80a0
Seo <kirin2211@naver.com>
2023-07-07 16:14:32 +0900

    enum and Dictionary

--- iOSCalculator/iOSCalculator/CalculatorBrain.swift
+++ iOSCalculator/iOSCalculator/CalculatorBrain.swift
@@ -15,15 +15,32 @@ class CalculatorBrain {
         accumulator = operand
     }
     
-    var operations : Dictionary<String,Double> = [
-        "π" : M_PI,
-        "e" : M_E
+    var operations : Dictionary<String,Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos)
     ]
     
+    enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation
+        case Equals
+    }
+    
     func performOperation(symbol : String){
-        if let constant = operations[symbol]{
-            accumulator = constant
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let associatedConstantValue) : accumulator = associatedConstantValue
+            case .UnaryOperation(let function) : accumulator = function(accumulator)
+            case .BinaryOperation : break
+            case .Equals : break
+            }
         }
+//        if let constant = operations[symbol]{
+//            accumulator = constant
+//        }
 //        switch symbol{
 //            case "π" : accumulator = M_PI
 //            case "√" : accumulator = sqrt(accumulator)
